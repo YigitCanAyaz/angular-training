ff289d9a873242daa97cac42795514d6bb4613c6
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-01-23 17:28:45 +0300

    Basics & Components & Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  // template: `<p>merhaba</p>
+  // <hr>
+  // <div style="background-color: aqua;">
+  // <app-home></app-home></div>
+  // `,
+  template:`
+  // property binding
+  <!-- <input type="text" [value]="title"/>
+  <app-home [pageName]="title"></app-home> -->
+
+  // Two Way Data Binding
+  <!-- <input type="text" [(ngModel)]="name" (ngModelChange)="onChange($event)"/><br/>
+  <input type="text" [(ngModel)]="name"/><br/>
+  {{name}} -->
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'proj1';
+  title = 'proj2';
+  name: string;
+
+  onChange($event) {
+    console.log("input değişti!!!");
+  }
 }
