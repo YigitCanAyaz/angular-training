9f26cc8216d341af0639b8e4a98f84bb5cd89a46
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-01-24 23:54:13 +0300

    Interpolation Syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,28 @@ import { Component } from '@angular/core';
   // <app-home></app-home></div>
   // `,
   template:`
-  // property binding
+  <!-- property binding -->
   <!-- <input type="text" [value]="title"/>
   <app-home [pageName]="title"></app-home> -->
 
-  // Two Way Data Binding
+  <!-- two Way Data Binding -->
   <!-- <input type="text" [(ngModel)]="name" (ngModelChange)="onChange($event)"/><br/>
   <input type="text" [(ngModel)]="name"/><br/>
   {{name}} -->
+
+  <!-- Interpolation Syntax -->
+  {{title}}
+  <p>{{person!.name}}</p>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'proj2';
-  name: string;
+  // title = 'proj2';
+  // name: string;
+
+  // Interpolation Syntax 
+  title: string = "proj3";
+  person : {name : string, surname: string};
 
   onChange($event) {
     console.log("input değişti!!!");
